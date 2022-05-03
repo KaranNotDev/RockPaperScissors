90308a634315e9a194c9a4e3ecef0995f6e4bcc6
Karan K <75043788+KaranNotDev@users.noreply.github.com>
2022-05-03 09:50:14 +0300

    RockPaperScissors.py
    The main code to run the game

---
+++ RockPaperScissors.py
@@ -0,0 +1,78 @@
+import random
+
+
+rock=("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+
+
+paper=("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+
+
+scissors=("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+
+lst=[rock, paper, scissors]
+
+print("WELCOME TO MY ROCK PAPER SCISSORS GAME \n MADE BY THE WORST PROGRAMMER @KARANNOTDEV\n")
+
+UserChoice=str(input("(R) for Rock, (P) for Paper, (S) for Scissors\n"))
+
+if (UserChoice=="R" or UserChoice=="r"):
+    UserChoice=rock
+    print("Ya choose: \n", UserChoice)
+    
+if (UserChoice=="P" or UserChoice=="P"):
+    UserChoice=paper
+    print("Ya choose: \n", UserChoice)
+        
+if (UserChoice=="S" or UserChoice=="S"):
+    UserChoice=scissors
+    print("Ya choose: \n", UserChoice)
+    
+else:
+    print("I SAID PRESS R, P OR S I DIDNT TELL YA TO PRESS ANYTHIN' ELSE")
+        
+RoboChoice=random.choice(lst)
+print("Robo choose: \n", RoboChoice)
+    
+print("And da winner is:")
+    
+if (RoboChoice==UserChoice):
+    print("No winner. That is a tie")
+        
+if (RoboChoice==rock and UserChoice==scissors):
+    print("Hah, take that! My program is better then you!")
+    
+if (RoboChoice==paper and UserChoice==rock):
+    print("Hah, take that! My program is better then you!")
+        
+if (RoboChoice==scissors and UserChoice==paper):
+    print("Hah, take that! My program is better then you!")
+        
+if (UserChoice==rock and RoboChoice==scissors):
+    print("Ah, okey you might be better then my program")
+        
+if (UserChoice==paper and RoboChoice==rock):
+    print("Ah, okey you might be better then my program")
+    
+if (UserChoice==scissors and RoboChoice==paper):
+    print("Ah, okey you might be better then my program")
